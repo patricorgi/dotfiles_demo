ba6423c1e9e8b748ac8ff7aba11f227471c04c84
patricorgi <patricorgi@gmail.com>
2024-05-25 20:34:58 -0400

    feat: telescope file ignore patterns

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "logs",
+        "%.md",
+        "%.root",
+        "%.gif",
+        "%.pdf",
+        "%.png",
+        "%.vcxproj",
+        "%.vcproj",
+        "%.notes",
+        "%.json",
+        "%.rst",
+        "%.bat",
+        "%.css",
+        "%.cxx",
+        "%.cmake",
+        "Online.*%.xml",
+      }
+    }
+  }
+}
+
